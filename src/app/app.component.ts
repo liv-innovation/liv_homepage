f63d0c15fcc9c4fa626499a3c5859b7783a86441
Arun <garunk09@gmail.com>
2024-04-06 00:16:53 +0530

    Initial layout - 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
+import { ProductsComponent } from './products/products.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
